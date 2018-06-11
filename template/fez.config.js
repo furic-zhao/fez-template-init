36ffab8d359a827c951ad91f2970330ae705e6e3
furic <furic@qq.com>
2018-06-11 13:33:33 +0800

    更新示例

--- template/fez.config.js
+++ template/fez.config.js
@@ -6,5 +6,38 @@
  */
 
 module.exports = {
-
+  {{#lint}}
+  /**
+   * 启用/禁用 eslint 检测代码
+   */
+  eslint: {
+    available: true
+  },
+  {{/lint}}
+  /**
+   * -------------------------------
+   * webpack配置
+   * -------------------------------
+   */
+  webpack: {
+    /**
+     * webpack.config.js
+     * 根据需求场景每个项目均可安装loader及plugin
+     */
+    config: {
+      module: {
+        rules: []
+      }
+    },
+    /**
+     * 用于提取合并公共模块
+     * {
+     *   target: '{打包名称}',
+     *   chunk: ['{chunk1名称}','{chunk2名称}','{chunk3名称}']
+     * }
+     */
+    extract: {
+      js: []
+    }
+  }
 }
