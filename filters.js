841105181ab7da800c2cd49e12793b813fa5cad7
furic <furic@qq.com>
2018-07-12 15:52:52 +0800

    更新示例

--- filters.js
+++ filters.js
@@ -1,8 +1,8 @@
 module.exports = {
     '.eslintrc.js': 'lint',
     '.eslintignore': 'lint',
-    'src/views/jquery/': "userModel && modelConfig.includes('jquery')",
-    'src/views/react/': "userModel && modelConfig.includes('react')",
-    'src/views/vue/': "userModel && modelConfig.includes('vue')",
+    'src/views/demo-jquery/': "userModel && modelConfig.includes('jquery')",
+    'src/views/demo-react/': "userModel && modelConfig.includes('react')",
+    'src/views/demo-vue/': "userModel && modelConfig.includes('vue')",
     'webpack.config.vue.js': "userModel && modelConfig.includes('vue')"
 }
